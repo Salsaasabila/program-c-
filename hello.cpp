d4968b38b5716b6ac94dc7eeea21f6fc1548ae76
Salsaasabila <111962363+Salsaasabila@users.noreply.github.com>
2022-09-01 15:41:30 +0700

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main()
+{
+    cout << "Nama: Salsa sabila"<<endl;
+    cout << " Kelas: X PPLG 1"<<endl;
+    cout << "==============="<<endl;
+    cout << "angka1 =10"<<endl;
+    cout << "angka2 =5"<<endl;
+    cout << " hasil:"<< 10 + 5<<endl;
+    return 0;
+}
